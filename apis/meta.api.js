ef770a6929995af74165f55d0e67f175cb51a9ba
Suyash Purwar <purwarsuyash06@gmail.com>
2023-02-22 17:14:11 +0530

    Move the API calls to .api.js files, fix bug in sendResultMenu, implement sendTextMessage function

---
+++ apis/meta.api.js
@@ -0,0 +1,69 @@
+import axios from 'axios';
+
+const sendAPICall = async (
+  endpoint,
+  method,
+  recipientNo,
+  msgType,
+  msgData
+) => {
+  const uri = `${process.env.META_API_URI}/${process.env.META_PHONE_ID}/${endpoint}`;
+  const requestBody = {
+    messaging_product: "whatsapp",
+    recipient_type: "individual",
+    to: recipientNo,
+    type: msgType,
+    [msgType]: msgData
+  };
+  const config = {
+    headers: {
+      Authorization: `Bearer ${process.env.META_ACCESS_TOKEN}`
+    }
+  };
+  let response;
+  switch (method) {
+    case 'post':
+      console.log(JSON.stringify(config, null, 2));
+      response = await axios.post(uri, requestBody, config);
+      break;
+    case 'get':
+      response = await axios.get(uri, requestBody, config);
+      break;
+    default:
+      console.log(`Invalid method": ${method}`);
+  }
+  return response;
+};
+
+export const sendTextMessage = async (recipientNo, message) => {
+  const text = {
+    preview_url: false,
+    body: message
+  };
+  const response = await sendAPICall(
+    'messages',
+    'post',
+    recipientNo,
+    'text',
+    text
+  );
+  console.log(response.data);
+  return response;
+};
+
+export const sendResultMenu = async (recipientNo) => {
+  const template = {
+    name: "result",
+    language: {
+      code: "en_US"
+    }
+  };
+  const response = await sendAPICall(
+    'messages',
+    'post',
+    recipientNo,
+    'template',
+    template
+  );
+  console.log(response.data)
+};
--- services/webhook.services.js
+++ services/webhook.services.js
@@ -1,41 +1,28 @@
-import axios from 'axios';
+import * as metaAPI from './../apis/meta.api.js';
 
 export const processMessage = async (msgInfo) => {
   const { value, field } = msgInfo;
 
   if (field !== 'messages') return res.sendStatus(403);
-  
-  if ('messages' in value) {
-    if (value.messages[0].type !== 'text') {
-      console.log('Only text messages are supported');
-      console.log(JSON.stringify(value, null, 3));
-      return;
-    }
-    const messageFrom = value.contacts[0].wa_id;
-    const message = value.messages[0].text.body;
 
-    // Feed the message to text classifier. In response, a keyword is returned
-    // Based on the keyword, a menu for that category is sent to the client
-    // Once the chosen option from all the subsequent menus is received
-    // A call to the LPU database will be made and the response will be parsed and sent to the client
-  
-    const keyword = classifyMsg(message);
-    switch (keyword) {
-      case 'result':
-        const res = await sendResultMenu(messageFrom);
+  if ('messages' in value) {
+    const messageFrom = +value.contacts[0].wa_id
+    const messageType = value.messages[0].type;
+
+    switch (messageType) {
+      case 'button':
+        console.log(JSON.stringify(value, null, 3));
         break;
-      case 'attendance':
-        console.log("Sending attendance menu");
-        break;
-      case 'warden':
-        console.log("Sending warden number");
+      case 'text':
+        const message = value.messages[0].text.body;
+        const keyword = classifyMsg(message);
+        await generateResponse(keyword, messageFrom);
         break;
       default:
-        console.log("Unknown keyword");
+        console.log(`Only text messages are supported. Received ${messageType}.`);
+        await metaAPI.sendTextMessage(messageFrom, `Sorry! I'm unable to process ${messageType} messages.`);
+        return;
     }
-
-    console.log(message, messageFrom);
-    console.log(`Intent ${keyword}`);
   } else if ('statuses' in value) {
     const messageStatus = value.statuses[0].status;
     const messageFrom = value.statuses[0].recipient_id;
@@ -51,35 +38,38 @@ export const processMessage = async (msgInfo) => {
  * Following is the mock implementation of text classifier
  */
 const classifyMsg = (msgText) => {
-  const KEYWORDS = ['result', 'marksheet', 'report', 'performance', 'attendance', 'warden', 'timetable', 'schedule'];
-
+  const dictionary = {
+    'result': ['result', 'marksheet', 'performance', 'report card'],
+    'attendance': ['attendance', 'attending', 'present', 'attended'],
+    'hostel_warden': ['warden', 'hostel head', 'supervisor']
+  };
+  
   let intent;
-  for (let keyword of KEYWORDS) {
-    if (msgText.toLowerCase().indexOf(keyword)) {
-      intent = keyword;
-      break;
+  for (let keywordClass in dictionary) {
+    for (let keyword of dictionary[keywordClass]) {
+      if (msgText.toLowerCase().indexOf(keyword) !== -1) {
+        intent = keywordClass;
+        break;
+      }
     }
   }
+
   return intent;
 };
 
-const sendResultMenu = async (recipientNo) => {
-  const endpoint = `${process.env.META_API_URI}/${process.env.META_PHONE_ID}/messages`;
-  const res = await axios.post(endpoint, {
-    messaging_product: "whatsapp",
-    recipient_type: "individual",
-    to: recipientNo,
-    type: "template",
-    template: {
-      name: "result",
-      language: {
-        code: "en_US"
-      }
-    }
-  }, {
-    headers: {
-      Authorization: `Bearer ${process.env.META_ACCESS_TOKEN}`
-    }
-  });
-  console.log(res);
-}
+const generateResponse = async (keyword, recipientNo) => {
+  switch (keyword) {
+    case 'result':
+      await metaAPI.sendResultMenu(recipientNo)
+      break;
+    case 'attendance':
+      console.log("Sending attendance menu");
+      break;
+    case 'hostel_warden':
+      console.log("Sending hostel warden details");
+      break;
+    default:
+      console.log("Unknown keyword");
+      await metaAPI.sendTextMessage(recipientNo, "Sorry, I didn't understood your message.")
+  }
+};
