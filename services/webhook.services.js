0c3c8f086bb77262c86a02af0423170848787937
Suyash Purwar <purwarsuyash06@gmail.com>
2023-02-23 20:58:27 +0530

    Complete getResult & getAttendance function and add prototype images

--- services/webhook.services.js
+++ services/webhook.services.js
@@ -52,7 +52,7 @@ export const processMessage = async (msgInfo) => {
  */
 const classifyMsg = (msgText) => {
   const dictionary = {
-    'result': ['result', 'marksheet', 'performance', 'report card'],
+    'result': ['result', 'marksheet', 'performance', 'report card', 'marks'],
     'attendance': ['attendance', 'attending', 'present', 'attended'],
     'hostel_warden': ['warden', 'hostel head', 'supervisor']
   };
@@ -83,45 +83,83 @@ const generateResponse = async (keyword, recipientNo) => {
       break;
     default:
       console.log("Unknown keyword");
-      await metaAPI.sendTextMessage(recipientNo, "Sorry, I didn't understood your message.")
+      await metaAPI.sendTextMessage(recipientNo, "Sorry, I didn't understood your message.");
   }
 };
 
 const getAttendance = async (recipientNo, choice) => {
   let response;
+  let message;
   switch (choice) {
     case "Today's Attendance":
       response = await Student.findOne(
         { contact: recipientNo },
         'id attendance.todays_attendance'
       );
+      const todays_attendance = response.attendance.todays_attendance.value;
+      message = `👉 Today's Attendance\n`;
+      for (let subjectAttendance of todays_attendance) {
+        message += `
+Timing: ${subjectAttendance.time}
+Subject Code: ${subjectAttendance.sub_code}
+Attendance: ${subjectAttendance.attendance ? 'present' : 'absent'}
+        `;
+      }
       break;
     case "Overall Attendance":
       response =  await Student.findOne(
         { contact: recipientNo },
         'id attendance.overall'
       );
+      const overall_attendance = response.attendance.overall;
+      message = `👉 Overall Attendance\n`;
+      for (let subjectAttendance of overall_attendance) {
+        message += `
+Subject Code: ${subjectAttendance.sub_code}
+Total Attendance: ${subjectAttendance.attendance}%
+        `;
+      }
       break;
     }
-    console.log(response);
+  await metaAPI.sendTextMessage(recipientNo, message);
 };
 
 const getResult = async (recipientNo, choice) => {
   const { id } = await Student.findOne({ contact: recipientNo }, 'id');
-  let response;
+  let response = await Result.findOne(
+    { id },
+    'overall_cgpa semester_result'
+  );
+  let message;
   switch (choice) {
     case "Last Semester":
-      response = await Result.findOne(
-        { id },
-        'overall_cgpa semester_result'
-      );
+      const lastSem = response.semester_result[response.semester_result.length-1];
+      message = `
+👉 *Result of last semester*
+
+Semester: ${lastSem.semester}\n`;
+      for (let semResultMarks of lastSem.marks) {
+        message += `
+Subject Code: ${semResultMarks.sub_code}
+Grade: ${semResultMarks.grade}
+`;
+      }
+      message += `\n💥 *Semester ${lastSem.semester} TGPA ${lastSem.tgpa}*`
       break;
     case "Previous All Semesters":
-      response = await Result.findOne(
-        { id },
-        'overall_cgpa semester_result'
-      );
+      message = `👉 *Result of all the semesters*\n\n\n`;
+      for (let semResult of response.semester_result) {
+        message += `✅ Semester ${semResult.semester} result:\n`;
+        for (let semResultMarks of semResult.marks) {
+          message += `
+Subject Code: ${semResultMarks.sub_code}
+Grade: ${semResultMarks.grade}
+`;
+        }
+        message += `\n🔥 *Semester ${semResult.semester} TGPA: ${semResult.tgpa}*\n\n`
+      }
+      message += `\n💥 *Total CGPA: ${response.overall_cgpa}*`;
       break;
-  }
-  console.log(response);
+    }
+  await metaAPI.sendTextMessage(recipientNo, message);
 }
