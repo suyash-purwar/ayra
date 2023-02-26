12e9f6303bdbcbf545591640be4793f84d079f96
Suyash Purwar <purwarsuyash06@gmail.com>
2023-02-26 14:10:44 +0530

    Add department.json and warden.json. Minor changes in publish-result

--- services/automated.services.js
+++ services/automated.services.js
@@ -7,16 +7,11 @@ export const firstHello = async (recipientNo) => {
 };
 
 export const publishResult = async () => {
-  // Query the Student doc, fetch the Reg. Id and phone no
-  // Query the Result doc by matching Reg. Id. and send the message to
-  // the associated phone numbers
-
-  // Add a temporary student filter for testing purposes
   const students = await Student.find({}, 'id contact');
-  console.log(students);
   for (let student of students) {
-    // Blocklisting all numbers other than these two
-    if (['919058765425', '919631733112'].indexOf(student.contact) === -1) continue
+    // Blocklisting all numbers other than ALLOWED_NUMBERS
+    const ALLOWED_NUMBERS = ['919058765425', '919631733112'];
+    if (ALLOWED_NUMBERS.indexOf(student.contact) === -1) continue
     let resultOfStudent = await Result.findOne({
       id: student.id
     }, 'overall_cgpa semester_result');
@@ -28,8 +23,6 @@ Subject Code: ${subject.sub_code}
 Grade: ${subject.grade}\n`;
     }
     message += `TGPA: ${lastSemester.tgpa}`;
-    // Send message to the student
-    console.log(message);
-    await metaAPI.sendTextMessage(+student.contact, message);
+    await  metaAPI.sendTextMessage(+student.contact, message);
   }
 };
